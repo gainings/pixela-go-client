eb653edba3759e50a122a35e069bfded69eca710
gainings <gain.beats@gmail.com>
2018-10-15 12:23:59 +0900

    Add UpdateToken func

--- pixela.go
+++ pixela.go
@@ -77,3 +77,49 @@ func (c Client) Register(username, token string, agree, notMinor string) error {
 	}
 	return nil
 }
+
+//UpdateToken is update user's token
+func (c Client) UpdateToken(username, oldToken, newToken string) error {
+	type updateInfo struct {
+		Token string `json:"newToken"`
+	}
+	ui := updateInfo{
+		Token: newToken,
+	}
+	uiJSON, err := json.Marshal(ui)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("PUT", c.URL+"/users/"+username, bytes.NewBuffer(uiJSON))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-USER-TOKEN", oldToken)
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return errors.New("return status code: " + res.Status)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	type RegisterResponse struct {
+		Message   string `json:"message"`
+		IsSuccess bool   `json:"isSuccess"`
+	}
+	rres := RegisterResponse{}
+	err = json.Unmarshal(body, &rres)
+	if err != nil {
+		return err
+	}
+	if !rres.IsSuccess {
+		return errors.New(rres.Message)
+	}
+	return nil
+}
