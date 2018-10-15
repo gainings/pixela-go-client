f427c04cd369e15cab6b069b805a392a711e5b12
gainings <gain.beats@gmail.com>
2018-10-15 12:09:51 +0900

    Add register func

--- pixela.go
+++ pixela.go
@@ -1,3 +1,79 @@
 package pixela
 
-const baseURL = "https://pixe.la/v1/"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+const baseURL = "https://pixe.la/v1"
+
+//Client is Client for access to pixe.la service
+type Client struct {
+	HTTPClient http.Client
+	URL        string
+}
+
+//NewClient is return Cilent
+func NewClient() *Client {
+	return &Client{
+		HTTPClient: http.Client{
+			Timeout: time.Duration(10) * time.Second,
+		},
+		URL: baseURL,
+	}
+}
+
+//Register is register new user
+func (c Client) Register(username, token string, agree, notMinor string) error {
+	type registerInfo struct {
+		Username string `json:"username"`
+		Token    string `json:"token"`
+		Agree    string `json:"agreeTermsOfService"`
+		NotMinor string `json:"notMinor"`
+	}
+	ri := registerInfo{
+		Username: username,
+		Token:    token,
+		Agree:    agree,
+		NotMinor: notMinor,
+	}
+	riJSON, err := json.Marshal(ri)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", c.URL+"/users", bytes.NewBuffer(riJSON))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return errors.New("return status code: " + res.Status)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	type RegisterResponse struct {
+		Message   string `json:"message"`
+		IsSuccess bool   `json:"isSuccess"`
+	}
+	rres := RegisterResponse{}
+	err = json.Unmarshal(body, &rres)
+	if err != nil {
+		return err
+	}
+	if !rres.IsSuccess {
+		return errors.New(rres.Message)
+	}
+	return nil
+}
