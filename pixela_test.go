1a8b448382156558d973870d260439854e2bf73a
gainings <gain.beats@gmail.com>
2018-10-15 12:35:55 +0900

    Add test for DeleteUser

--- pixela_test.go
+++ pixela_test.go
@@ -2,30 +2,44 @@ package pixela
 
 import (
 	"testing"
-	"time"
 )
 
 func TestRegister(t *testing.T) {
 	c := NewClient()
-	err := c.Register("test-user"+time.Now().String(), "testtest", "no", "yes")
+	//Initialize test account
+	c.DeleteUser("test-gainings", "testtest")
+	c.DeleteUser("test-gainings", "testhogehoge")
+
+	err := c.Register("test-gainings", "testtest", "no", "yes")
 	if err == nil {
 		t.Fatalf("want err, but got nil")
 	}
 
-	err = c.Register("test-user"+time.Now().String(), "testtest", "yes", "no")
+	err = c.Register("test-gainings", "testtest", "yes", "no")
 	if err == nil {
 		t.Fatalf("want err, but got nil")
 	}
 
-	err = c.Register("test-user"+time.Now().String(), "testtest", "yes", "yes")
+	err = c.Register("test-gainings", "testtest", "yes", "yes")
 	if err != nil {
 		t.Errorf("want nil, got %#v", err)
 	}
-}
+	err = c.Register("test-gainings", "testtest", "yes", "yes")
+	if err == nil {
+		t.Fatalf("want err, but got nil")
+	}
 
-func TestUpdate(t *testing.T) {
-	c := NewClient()
-	err := c.UpdateToken("test-gainings", "testtest", "testhogehoge")
+	err = c.UpdateToken("test-gainings", "testtest", "testhogehoge")
+	if err != nil {
+		t.Errorf("want nil, got %v", err)
+	}
+
+	err = c.DeleteUser("test-gainings", "testtest")
+	if err == nil {
+		t.Fatalf("want err, but got nil")
+	}
+
+	err = c.DeleteUser("test-gainings", "testhogehoge")
 	if err != nil {
 		t.Errorf("want nil, got %v", err)
 	}
